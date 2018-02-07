42a7bf1ae3899aa8615227fd2df67e1165f09249
giulio <giuliop@gmail.com>
2018-02-07 11:20:36 +0100

    testing around

--- test.c
+++ test.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stddef.h>
 
 int main() {
-  unsigned const * n;
-  n  = &(unsigned){5};
-  printf("%u\n", *n);
-  /**n = 10;*/
-  /*printf("%u\n", *n);*/
+  int c1 = 'c';
+  char c2 = 'c';
+  char * s1 = "ciao";
+  char * s2 = &(s1[0]);
+  char * s3 = &c2;
+  printf("%d", c1);
+  printf("\n");
+  printf("%c",c2);
+  printf("\n");
+  printf("%s", s1);
+  printf("\n");
+  printf("%s", s2);
+  printf("\n");
+  printf("%c", *s3);
+  printf("\n");
+  /*printf("%lU\n", sizeof(int));*/
+  /*printf("%lU\n", sizeof(int));*/
+  /*printf("%lU\n", sizeof(char));*/
+  /*printf("%lU\n", sizeof(wchar_t));*/
 }
