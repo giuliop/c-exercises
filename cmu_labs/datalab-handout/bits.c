0de6b93983b9d50465b2e5cca86fb2d738aa84d5
giulio <giuliop@gmail.com>
2018-04-01 22:51:53 +0200

    up to function float_i2f passing tests

--- cmu_labs/datalab-handout/bits.c
+++ cmu_labs/datalab-handout/bits.c
@@ -139,6 +139,7 @@ NOTES:
  *   Rating: 1
  */
 int bitAnd(int x, int y) {
+	/* We do x AND y as NOT ((NOT x) OR (NOT y)) */
 	x = ~x;
 	y = ~y;
   return ~(x|y);
@@ -152,15 +153,13 @@ int bitAnd(int x, int y) {
  *   Rating: 2
  */
 int getByte(int x, int n) {
-
-
-
-
-
-
-
-  return 2;
-
+	/* We make n as n*8 to have the bits to right shift x to put the wanted byte
+	 * as first byte that we then extract with AND and the mask 0xff */
+	n += n;
+	n += n;
+	n += n;
+	x >>= n;
+	return (x & 0xff);
 }
 /* 
  * logicalShift - shift x to the right by n, using a logical shift
@@ -171,7 +170,14 @@ int getByte(int x, int n) {
  *   Rating: 3 
  */
 int logicalShift(int x, int n) {
-  return 2;
+	/* We extract the sign bit, replace it with 0, right shift the number as as logical
+	 * shift since the sign bit is zero, create a mask as 0..0x0..0 where x is the
+	 * sign bit so we can reset it to the original value with an OR */
+	int mask = (x>>31) & 1;	// sign bit of x
+	mask <<= (31+(~n+1)); // put sign bit in position after right shift
+	x &= ~(1<<31); // set sign bit to zero
+	x >>= n; // perform shift as if logical
+	return x|mask;
 }
 /*
  * bitCount - returns count of number of 1's in word
@@ -181,7 +187,26 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+	/* Naive implementation of Hamming weigth algorithm. We first sum every two
+	 * bits together by adding x to its right shift by one each masked by 010101..
+	 * so that we clear every other bit, and so on until we sum all bits
+	 */
+	int mask = 0x55;									//													  01010101
+	mask = mask + (mask<<8);					//									 01010101 01010101
+	mask = mask + (mask<<16);					// 01010101 01010101 01010101 01010101 
+	x = (x & mask) + ((x>>1) & mask);
+	mask = 0x33;											//														00110011
+	mask = mask + (mask<<8);					//									 00110011 00110011
+	mask = mask + (mask<<16);					// 00110011 00110011 00110011 00110011 
+	x = (x & mask) + ((x>>2) & mask);
+	mask = 0x0f;											//														00001111
+	mask = mask + (mask<<8);					//									 00001111 00001111
+	mask = mask + (mask<<16);					// 00001111 00001111 00001111 00001111  
+	x = (x & mask) + ((x>>4) & mask);
+	x += x>>8;
+	x += x>>16;
+	mask = 0xff;
+  return x & mask;
 }
 /* 
  * bang - Compute !x without using !
@@ -191,7 +216,21 @@ int bitCount(int x) {
  *   Rating: 4 
  */
 int bang(int x) {
-  return 2;
+	/* The idea is to OR all bits among themselves, and negate the resulting bit.
+	 * We of it by right shifting x by half each time (16 then 8, 4, 2, 1) and ORing
+	 * it with itself. Then we negate and return the leasts significant bit
+	 */
+	int x1 = x>>16;
+	x |= x1;
+	x1 = x>>8;
+	x |= x1;
+	x1 = x>>4;
+	x |= x1;
+	x1 = x>>2;
+	x |= x1;
+	x1 = x>>1;
+	x |= x1;
+	return (~x) & 1;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -200,7 +239,7 @@ int bang(int x) {
  *   Rating: 1
  */
 int tmin(void) {
-  return 2;
+  return 1<<31;
 }
 /* 
  * fitsBits - return 1 if x can be represented as an 
@@ -212,7 +251,14 @@ int tmin(void) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-  return 2;
+	/* Positive numbers can be represented if have no ones after the n-1 least
+	 * signtificative bits; negative ones if no zeroes.
+	 * By right shiftinf x by n-1 and adding the sign bit we get all 0s if
+	 * the number is representable, so we just return its negation
+	 */
+	x >>= n + (~1+1);
+	x += (x>>31) & 1;
+  return !x;
 }
 /* 
  * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
@@ -223,7 +269,15 @@ int fitsBits(int x, int n) {
  *   Rating: 2
  */
 int divpwr2(int x, int n) {
-    return 2;
+	/* For positive numbers right shifting already rounds towards zero
+	 * For negative ones we need to add 1 if the number is not a multiple
+	 * of 2^n that is if it has 1s in the least significative n bits
+	 */
+	int mask = ~(~0 << n);			// n 1s
+	int round = !!(x & mask);
+	x >>= n;
+	x += (x>>31) & round;
+	return x;
 }
 /* 
  * negate - return -x 
@@ -233,7 +287,8 @@ int divpwr2(int x, int n) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+	/* In two complement -x = ~x+1 */
+  return (~x+1);
 }
 /* 
  * isPositive - return 1 if x > 0, return 0 otherwise 
@@ -243,7 +298,9 @@ int negate(int x) {
  *   Rating: 3
  */
 int isPositive(int x) {
-  return 2;
+	/* x is positive if its sign bit is zero and it is not zero */
+	int sign = (x>>31) & 1;
+  return !sign & !!x;		// & !!x to account for the case x = 0
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -253,7 +310,16 @@ int isPositive(int x) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+	/* x is less or equal in one of these cases:
+	 * 1. x is neg and y is pos
+	 * 2. x and y are equal
+	 * 3. x and y have same sign and their difference is negative
+	 */
+	int delta = x + (~y+1); // could overlfow if x and y different sign
+	int x_neg = (x>>31) & 1;
+	int y_neg = (y>>31) & 1;
+	int delta_neg = (delta>>31) & 1;
+	return (x_neg & !y_neg) | !delta | ((!(x_neg^y_neg)) & delta_neg); 
 }
 /*
  * ilog2 - return floor(log base 2 of x), where x > 0
@@ -263,7 +329,37 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4
  */
 int ilog2(int x) {
-  return 2;
+	/* The answer is 31 - the number of leading 0 of x (how many times we can right
+	 * shift x, or divide it by 2). To compute it we propagate the most significant
+	 * 1 to the right by ORing x with x right shifted by 1, 2, 4...
+	 * Then we can count 1s with the Hamming distance algo and subtract 1
+	 * which is the same as 31 - leading 0s
+	*/
+	int mask = 0x55;
+
+	x = x | x>>1;
+	x = x | x>>2;
+	x = x | x>>4;
+	x = x | x>>8;
+	x = x | x>>16;
+	
+	mask = mask + (mask<<8);
+	mask = mask + (mask<<16);
+	x = (x & mask) + ((x>>1) & mask);
+	mask = 0x33;
+	mask = mask + (mask<<8);
+	mask = mask + (mask<<16);
+	x = (x & mask) + ((x>>2) & mask);
+	mask = 0x0f;
+	mask = mask + (mask<<8);
+	mask = mask + (mask<<16);
+	x = (x & mask) + ((x>>4) & mask);
+	x += x>>8;
+	x += x>>16;
+	mask = 0xff;
+  x &= mask;
+
+  return x + (~1+1);
 }
 /* 
  * float_neg - Return bit-level equivalent of expression -f for
@@ -277,7 +373,15 @@ int ilog2(int x) {
  *   Rating: 2
  */
 unsigned float_neg(unsigned uf) {
- return 2;
+	/* We recognize NaN by checking if the exp is all 1s and the frac field is not
+	 * all 0s. If not NaN we simply toggle the sign bit
+	 */
+	int exp = (uf>>23) & 0xff;
+	int frac = uf & 0x7fffff;
+	if ((exp == 0xff) && frac) {
+		return uf;
+	}
+	return uf ^ (1<<31);
 }
 /* 
  * float_i2f - Return bit-level equivalent of expression (float) x
@@ -289,7 +393,58 @@ unsigned float_neg(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+	/* We construct the float as follows:
+	 * 1. Sign: we simply extract the sign from int; if it is negative we transform
+	 *	  x in positive
+	 * 2. Exp: 127 (bias) + position of most significant bit of x (except sign bit)
+	 *	  where the least signigicant bit is position 0; potentially + 1 if rounding
+	 *	  happen (see below)
+	 * 3. Frac: the (up to) 23 most significant bits of x (after the sign bit and
+	 *    the initial 0s, and dropping the leading 1); potentially padded by 0
+	 *    to the right to get 23 bits; potentially + 1 if rounding happen (see below)
+	 * 4. Rounding: if x has more than 23 significants bits (excluding sign bit) we
+	 *	  need to round. There are three cases based on the pattern of the extra bits:
+	 *			a. 0x..x -> round-down;
+	 *			b. 1x1x. -> round-up: frac++ and if frac was all 1s, exp++ and frac = 0
+	 *			c. 100.0 -> if last significant bit we keep is 0 round-donw, if 1 round-up
+	 * 5. Corner cases are x = 0 (return x) and x = INT_MIN (return 0xcf000000)
+	 */
+	int sign = (x>>31) & 1;
+  int n = 30;
+	int exp = 0;
+	int frac = 0;
+	int bits_lost = 0;
+	int last_bit_kept = 0;
+	int round_up = 0; // 1 round-up; 0 round-donw
+	int res = 0;
+	if (!x) return x;
+	if (x == 0x80000000) return 0xcf000000;
+	if (sign) x = -x;
+	while (!(x>>n)) --n; // n is position of most significative bit
+	exp = 127 + n;
+	frac = ((n > 23) ? (x>>(n-23)) : (x & ((1<<n)-1))<<(23-n)) & 0x7fffff;
+	/*printf("%d \n", sign);*/
+	/*printf("\n");*/
+	/*printf("x: %x \n", x);*/
+	/*printf("n: %d \n", n);*/
+	/*printf("%d \n", exp);*/
+	/*printf("%d \n", frac);*/
+  res = (sign<<31) | (exp<<23) | frac;
+	if (n > 23) {
+		bits_lost = x & ((1<<(n-23))-1);	// mask is 00..1..1 with n-23 1s (lost bits)
+			/*printf("bits_lost: %x \n", bits_lost);*/
+		last_bit_kept = (x>>(n-23)) & 1;
+			/*printf("last_bit_kept: %x \n", last_bit_kept);*/
+			/*printf("bits_lost>>(n-25): %x \n", bits_lost>>(n-25));*/
+		round_up = (bits_lost > (1<<n-24)) ||
+			((bits_lost == (1<<(n-24))) && last_bit_kept);
+		if (round_up) {
+			/*printf("Round-up! \n");*/
+			++res;
+		}
+	} 
+	/*printf("\n");*/
+	return res;
 }
 /* 
  * float_twice - Return bit-level equivalent of expression 2*f for
