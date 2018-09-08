4767f6e1077420e001bde90d1d430c3013f137f8
giulio <giuliop@gmail.com>
2018-09-08 20:34:03 +0200

    working on it

--- cmu_labs/cache/trans.c
+++ cmu_labs/cache/trans.c
@@ -19,500 +19,127 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+
+void trans(int m, int n, int jj, int ii, int M, int N, int A[N][M], int B[M][N]) {
+	m += jj;
+	n += ii;
+	for (; jj<m; jj++) {
+		for (int i=ii; i<n; i++) {
+			B[jj][i] = A[i][jj];
+		}
+	}
+}
+
+void copy(int m, int n, int j1, int i1, int j2, int i2, int M, int N, int A[N][M], int B[M][N]) {
+	m += j1;
+	n += i1;
+	for (; j1<m; j1++, j2++) {
+		int i11=i1;
+		int i22=i2;
+		for (; i11<n; i11++, i22++) {
+			B[i22][j2] = A[i11][j1];
+		}
+	}
+}
+
+void copytrans(int m, int n, int j1, int i1, int j2, int i2, int M, int N, int A[N][M], int B[M][N]) {
+	m += j1;
+	n += i1;
+	for (; i1<m; i1++, i2++) {
+		int j11=j1;
+		int j22=j2;
+		for (; j11<n; j11++, j22++) {
+			B[j22][i2] = A[i1][j11];
+		}
+	}
+}
+
+void trans_diag_4x4(int j, int i, int M, int N, int A[N][M], int B[M][N]) {
+	int t1, t2, t3, t4, t5, t6, t7, t8;
+	t1 = A[i+2][j];
+	t2 = A[i+2][j+1];
+	t3 = A[i+3][j];
+	t4 = A[i+3][j+1];
+	t5 = A[i+2][j+2];
+	t6 = A[i+2][j+3];
+	t7 = A[i+3][j+2];
+	t8 = A[i+3][j+3];
+	trans(2, 2, j+2, i, M, N, A, B); 
+	B[j+2][i+2] = t5;
+	B[j+3][i+2] = t6;
+	B[j+2][i+3] = t7;
+	B[j+3][i+3] = t8;
+	t5 = A[i][j];
+	t6 = A[i][j+1];
+	t7 = A[i+1][j];
+	t8 = A[i+1][j+1];
+	B[j][i+2] = t1;
+	B[j+1][i+2] = t2;
+	B[j][i+3] = t3;
+	B[j+1][i+3] = t4;
+	B[j][i] = t5;
+	B[j+1][i] = t6;
+	B[j][i+1] = t7;
+	B[j+1][i+1] = t8;
+}
+
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-		int i, j;
-		int t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-
-		for (i = 0; i < (N/4) * 4; i+=4) {
-				for (j = 0; j < (M/8) * 8; j+=8) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i+1][j];
-								t5 = A[i+1][j+1];
-								t6 = A[i+1][j+2];
-								t7 = A[i+2][j];
-								t8 = A[i+2][j+1];
-								t9 = A[i+2][j+2];
-								t10 = A[i+3][j];
-
-								B[j][i] = t1;
-								B[j][i+1] = t4;
-								B[j][i+2] = t7;
-								B[j][i+3] = t10;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t5;
-								B[j+1][i+2] = t8;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t6;
-								B[j+2][i+2] = t9;
-
-								t1 = A[i+3][j+1];
-								t2 = A[i+3][j+2];
-								B[j+1][i+3] = t1;
-								B[j+2][i+3] = t2;
-
-								t1 = A[i][j+3];
-								t2 = A[i][j+4];
-								t3 = A[i][j+5];
-								t4 = A[i+1][j+3];
-								t5 = A[i+1][j+4];
-								t6 = A[i+1][j+5];
-								t7 = A[i+2][j+3];
-								t8 = A[i+2][j+4];
-								t9 = A[i+2][j+5];
-								t10 = A[i+3][j+3];
-
-								B[j+3][i] = t1;
-								B[j+3][i+1] = t4;
-								B[j+3][i+2] = t7;
-								B[j+3][i+3] = t10;
-								B[j+4][i] = t2;
-								B[j+4][i+1] = t5;
-								B[j+4][i+2] = t8;
-								B[j+5][i] = t3;
-								B[j+5][i+1] = t6;
-								B[j+5][i+2] = t9;
-
-								t1 = A[i+3][j+4];
-								t2 = A[i+3][j+5];
-								B[j+4][i+3] = t1;
-								B[j+5][i+3] = t2;
-
-								t1 = A[i][j+6];
-								t2 = A[i][j+7];
-								t3 = A[i+1][j+6];
-								t4 = A[i+1][j+7];
-								t5 = A[i+2][j+6];
-								t6 = A[i+2][j+7];
-								t7 = A[i+3][j+6];
-								t8 = A[i+3][j+7];
-
-								B[j+6][i] = t1;
-								B[j+6][i+1] = t3;
-								B[j+6][i+2] = t5;
-								B[j+6][i+3] = t7;
-								B[j+7][i] = t2;
-								B[j+7][i+1] = t4;
-								B[j+7][i+2] = t6;
-								B[j+7][i+3] = t8;
-								if ((N == 67) && (M == 61)) {
-										t1 = A[i][56];
-										t2 = A[i][57];
-										t3 = A[i][58];
-										t4 = A[i][59];
-										t5 = A[i][60];
-										t6 = A[i+1][56];
-										t7 = A[i+1][57];
-										t8 = A[i+1][58];
-										t9 = A[i+1][59];
-										t10 = A[i+1][60];
-										B[56][i] = t1;
-										B[56][i+1] = t6;
-										B[57][i] = t2;
-										B[57][i+1] = t7;
-										B[58][i] = t3;
-										B[58][i+1] = t8;
-										B[59][i] = t4;
-										B[59][i+1] = t9;
-										B[60][i] = t5;
-										B[60][i+1] = t10;
-
-										t1 = A[i+2][56];
-										t2 = A[i+2][57];
-										t3 = A[i+2][58];
-										t4 = A[i+2][59];
-										t5 = A[i+2][60];
-										t6 = A[i+3][56];
-										t7 = A[i+3][57];
-										t8 = A[i+3][58];
-										t9 = A[i+3][59];
-										t10 = A[i+3][60];
-										B[56][i+2] = t1;
-										B[56][i+3] = t6;
-										B[57][i+2] = t2;
-										B[57][i+3] = t7;
-										B[58][i+2] = t3;
-										B[58][i+3] = t8;
-										B[59][i+2] = t4;
-										B[59][i+3] = t9;
-										B[60][i+2] = t5;
-										B[60][i+3] = t10;
-								}
-				}
+	if (M == 32 && N == 32) {
+return;
+		for (int i=0; i<N; i+=8) {
+			for (int j=0; j<M; j+=8) {
+				trans(4, 4, j+4, i, M, N, A, B); 
+				trans_diag_4x4(j, i, M, N, A, B); 
+				trans(4, 4, j, i+4, M, N, A, B); 
+				trans_diag_4x4(j+4, i+4, M, N, A, B); 
+			}
 		}
-		if ((N == 67) && (M == 61)) {
-				for (j = 0; j < 56; j+=8) {
-								t1 = A[64][j];
-								t2 = A[64][j+1];
-								t3 = A[64][j+2];
-								t4 = A[65][j];
-								t5 = A[65][j+1];
-								t6 = A[65][j+2];
-								t7 = A[66][j];
-								t8 = A[66][j+1];
-								t9 = A[66][j+2];
-
-								B[j][64] = t1;
-								B[j][65] = t4;
-								B[j][66] = t7;
-								B[j+1][64] = t2;
-								B[j+1][65] = t5;
-								B[j+1][66] = t8;
-								B[j+2][64] = t3;
-								B[j+2][65] = t6;
-								B[j+2][66] = t9;
-
-								t1 = A[64][j+3];
-								t2 = A[64][j+4];
-								t3 = A[64][j+5];
-								t4 = A[65][j+3];
-								t5 = A[65][j+4];
-								t6 = A[65][j+5];
-								t7 = A[66][j+3];
-								t8 = A[66][j+4];
-								t9 = A[66][j+5];
-
-								B[j+3][64] = t1;
-								B[j+3][65] = t4;
-								B[j+3][66] = t7;
-								B[j+4][64] = t2;
-								B[j+4][65] = t5;
-								B[j+4][66] = t8;
-								B[j+5][64] = t3;
-								B[j+5][65] = t6;
-								B[j+5][66] = t9;
-
-								t1 = A[64][j+6];
-								t2 = A[64][j+7];
-								t3 = A[65][j+6];
-								t4 = A[65][j+7];
-								t5 = A[66][j+6];
-								t6 = A[66][j+7];
-
-								B[j+6][64] = t1;
-								B[j+6][65] = t3;
-								B[j+6][66] = t5;
-								B[j+7][64] = t2;
-								B[j+7][65] = t4;
-								B[j+7][66] = t6;
+	}
+	if (M == 64 && N == 64) {
+		for (int i=0; i<N; i+=8) {
+			for (int j=0; j<M; j+=8) {
+				trans_diag_4x4(j, i, M, N, A, B); 
+//				copy(4, 4, j+4, i, 60, 60, M, N, A, B);
+//				trans(4, 4, j, i+4, M, N, A, B);
+				int jj = 60;
+				for (int z=0; z<4; ++z) {
+					int t1 = A[z+i][j+4];
+					int t2 = A[z+i][j+5];
+					int t3 = A[z+i][j+6];
+					int t4 = A[z+i][j+7];
+					trans(4, 1, j, z+i+4, M, N, A, B);
+					if (j == 56 && i != 56) jj = 60;
+					B[z+60][jj] = t1;
+					B[z+60][jj+1] = t2;
+					B[z+60][jj+2] = t3;
+					B[z+60][jj+3] = t4;
 				}
-								t1 = A[64][56];
-								t2 = A[64][57];
-								t3 = A[64][58];
-								t4 = A[64][59];
-								t5 = A[64][60];
-								t6 = A[65][56];
-								t7 = A[65][57];
-								t8 = A[65][58];
-								t9 = A[65][59];
-								t10 = A[65][60];
-								B[56][64] = t1;
-								B[56][65] = t6;
-								B[57][64] = t2;
-								B[57][65] = t7;
-								B[58][64] = t3;
-								B[58][65] = t8;
-								B[59][64] = t4;
-								B[59][65] = t9;
-								B[60][64] = t5;
-								B[60][65] = t10;
-			
-								t1 = A[66][56];
-								t2 = A[66][57];
-								t3 = A[66][58];
-								t4 = A[66][59];
-								t5 = A[66][60];
-								B[56][66] = t1;
-								B[57][66] = t2;
-								B[58][66] = t3;
-								B[59][66] = t4;
-								B[60][66] = t5;
+				copytrans(4, 4, jj, 60, j+4, i, M, N, B, B);
+				trans_diag_4x4(j+4, i+4, M, N, A, B); 
+			}
 		}
+	}
+	return;
 }
 
-/* 
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+// You can define additional transpose functions below.
 
-char transpose_2x4_block_desc[] = "Transpose 2x4 block";
-void transpose_2x4_block(int M, int N, int A[N][M], int B[M][N])
+char transpose_8x8_desc[] = "Transpose 8x8 block";
+void transpose_8x8(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j;
-		int t1, t2, t3, t4, t5, t6, t7, t8; //, t9;
-
-    for (i = 0; i < N; i+=2) {
-				for (j = 0; j < M; j+=4) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i][j+3];
-								t5 = A[i+1][j];
-								t6 = A[i+1][j+1];
-								t7 = A[i+1][j+2];
-								t8 = A[i+1][j+3];
-
-								B[j][i] = t1;
-								B[j][i+1] = t5;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t6;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t7;
-								B[j+3][i] = t4;
-								B[j+3][i+1] = t8;
+	for (int ii=0; ii<N; ii+=4) {
+		for (int jj=0; jj<M; jj+=4) {
+			for (int i=ii; i<ii+4; i++) {
+				for (int j=jj; j<jj+4; j++) {
+					B[j][i] = A[i][j];
 				}
+			}
 		}
+	}
 }
 
-char transpose_2x2_block_desc[] = "Transpose 2x2 block";
-void transpose_2x2_block(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
-		int t1, t2, t3, t4;
-
-    for (i = 0; i < N; i+=2) {
-				for (j = 0; j < M; j+=2) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i+1][j];
-								t4 = A[i+1][j+1];
-
-								B[j][i] = t1;
-								B[j][i+1] = t3;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t4;
-				}
-		}
-}
-
-char transpose_2x3_2x5_block_desc[] = "Transpose 2x3_2x5 block";
-void transpose_2x3_2x5_block(int M, int N, int A[N][M], int B[M][N])
-{
-		int i,j;
-		int t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-
-    for (i = 0; i < N; i+=2) {
-				for (j = 0; j < M; j+=8) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i+1][j];
-								t5 = A[i+1][j+1];
-								t6 = A[i+1][j+2];
-								B[j][i] = t1;
-								B[j][i+1] = t4;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t5;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t6;
-
-								t1 = A[i][j+3];
-								t2 = A[i][j+4];
-								t3 = A[i][j+5];
-								t4 = A[i][j+6];
-								t5 = A[i][j+7];
-								t6 = A[i+1][j+3];
-								t7 = A[i+1][j+4];
-								t8 = A[i+1][j+5];
-								t9 = A[i+1][j+6];
-								t10 = A[i+1][j+7];
-								B[j+3][i] = t1;
-								B[j+3][i+1] = t6;
-								B[j+4][i] = t2;
-								B[j+4][i+1] = t7;
-								B[j+5][i] = t3;
-								B[j+5][i+1] = t8;
-								B[j+6][i] = t4;
-								B[j+6][i+1] = t9;
-								B[j+7][i] = t5;
-								B[j+7][i+1] = t10;
-				}
-		}
-}
-
-char transpose_3x3_3x3_3x2_block_desc[] = "Transpose 3x3_3x3_3x2 block";
-void transpose_3x3_3x3_3x2_block(int M, int N, int A[N][M], int B[M][N])
-{
-		int i,j;
-		int t1, t2, t3, t4, t5, t6, t7, t8, t9;
-
-    for (i = 0; i < 30; i+=3) {
-				for (j = 0; j < M; j+=8) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i+1][j];
-								t5 = A[i+1][j+1];
-								t6 = A[i+1][j+2];
-								t7 = A[i+2][j];
-								t8 = A[i+2][j+1];
-								t9 = A[i+2][j+2];
-								B[j][i] = t1;
-								B[j][i+1] = t4;
-								B[j][i+2] = t7;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t5;
-								B[j+1][i+2] = t8;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t6;
-								B[j+2][i+2] = t9;
-
-								t1 = A[i][j+3];
-								t2 = A[i][j+4];
-								t3 = A[i][j+5];
-								t4 = A[i+1][j+3];
-								t5 = A[i+1][j+4];
-								t6 = A[i+1][j+5];
-								t7 = A[i+2][j+3];
-								t8 = A[i+2][j+4];
-								t9 = A[i+2][j+5];
-								B[j+3][i] = t1;
-								B[j+3][i+1] = t4;
-								B[j+3][i+2] = t7;
-								B[j+4][i] = t2;
-								B[j+4][i+1] = t5;
-								B[j+4][i+2] = t8;
-								B[j+5][i] = t3;
-								B[j+5][i+1] = t6;
-								B[j+5][i+2] = t9;
-
-								t1 = A[i][j+6];
-								t2 = A[i][j+7];
-								t3 = A[i+1][j+6];
-								t4 = A[i+1][j+7];
-								t5 = A[i+2][j+6];
-								t6 = A[i+2][j+7];
-								B[j+6][i] = t1;
-								B[j+6][i+1] = t3;
-								B[j+6][i+2] = t5;
-								B[j+7][i] = t2;
-								B[j+7][i+1] = t4;
-								B[j+7][i+2] = t6;
-				}
-		}
-    for (i = 30; i < N; i+=2) {
-				for (j = 0; j < M; j+=4) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i][j+3];
-								t5 = A[i+1][j];
-								t6 = A[i+1][j+1];
-								t7 = A[i+1][j+2];
-								t8 = A[i+1][j+3];
-
-								B[j][i] = t1;
-								B[j][i+1] = t5;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t6;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t7;
-								B[j+3][i] = t4;
-								B[j+3][i+1] = t8;
-				}
-		}
-}
-
-char transpose_4x3_3_2_block_desc[] = "Transpose 4x3_3_2 block";
-void transpose_4x3_3_2_block(int M, int N, int A[N][M], int B[M][N])
-{
-		int t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-
-    for (int i = 0; i < (N/4) * 4; i+=4) {
-				for (int j = 0; j < (M/8) * 8; j+=8) {
-								t1 = A[i][j];
-								t2 = A[i][j+1];
-								t3 = A[i][j+2];
-								t4 = A[i+1][j];
-								t5 = A[i+1][j+1];
-								t6 = A[i+1][j+2];
-								t7 = A[i+2][j];
-								t8 = A[i+2][j+1];
-								t9 = A[i+2][j+2];
-								t10 = A[i+3][j];
-
-								B[j][i] = t1;
-								B[j][i+1] = t4;
-								B[j][i+2] = t7;
-								B[j][i+3] = t10;
-								B[j+1][i] = t2;
-								B[j+1][i+1] = t5;
-								B[j+1][i+2] = t8;
-								B[j+2][i] = t3;
-								B[j+2][i+1] = t6;
-								B[j+2][i+2] = t9;
-
-								t1 = A[i+3][j+1];
-								t2 = A[i+3][j+2];
-								B[j+1][i+3] = t1;
-								B[j+2][i+3] = t2;
-
-								t1 = A[i][j+3];
-								t2 = A[i][j+4];
-								t3 = A[i][j+5];
-								t4 = A[i+1][j+3];
-								t5 = A[i+1][j+4];
-								t6 = A[i+1][j+5];
-								t7 = A[i+2][j+3];
-								t8 = A[i+2][j+4];
-								t9 = A[i+2][j+5];
-								t10 = A[i+3][j+3];
-
-								B[j+3][i] = t1;
-								B[j+3][i+1] = t4;
-								B[j+3][i+2] = t7;
-								B[j+3][i+3] = t10;
-								B[j+4][i] = t2;
-								B[j+4][i+1] = t5;
-								B[j+4][i+2] = t8;
-								B[j+5][i] = t3;
-								B[j+5][i+1] = t6;
-								B[j+5][i+2] = t9;
-
-								t1 = A[i+3][j+4];
-								t2 = A[i+3][j+5];
-								B[j+4][i+3] = t1;
-								B[j+5][i+3] = t2;
-
-								t1 = A[i][j+6];
-								t2 = A[i][j+7];
-								t3 = A[i+1][j+6];
-								t4 = A[i+1][j+7];
-								t5 = A[i+2][j+6];
-								t6 = A[i+2][j+7];
-								t7 = A[i+3][j+6];
-								t8 = A[i+3][j+7];
-
-								B[j+6][i] = t1;
-								B[j+6][i+1] = t3;
-								B[j+6][i+2] = t5;
-								B[j+6][i+3] = t7;
-								B[j+7][i] = t2;
-								B[j+7][i+1] = t4;
-								B[j+7][i+2] = t6;
-								B[j+7][i+3] = t8;
-				}
-		}
-}
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
- */
-char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
-}
 
 /*
  * registerFunctions - This function registers your transpose
@@ -527,11 +154,7 @@ void registerFunctions()
 		registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-	/*registerTransFunction(transpose_3x3_3x3_3x2_block, transpose_3x3_3x3_3x2_block_desc); */
-	/*registerTransFunction(transpose_2x3_2x5_block, transpose_2x3_2x5_block_desc); */
-	/*registerTransFunction(transpose_4x3_3_2_block, transpose_4x3_3_2_block_desc); */
-	/*registerTransFunction(transpose_2x4_block, transpose_2x4_block_desc); */
-	/*registerTransFunction(transpose_2x2_block, transpose_2x2_block_desc); */
+//	registerTransFunction(transpose_8x8, transpose_8x8_desc);
 
 }
 
